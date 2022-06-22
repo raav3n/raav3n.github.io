58a9b3c40273620d0a8f1afa30d1af9299f32edf
raav3n <frank.mancia@outlook.com>
2022-06-22 16:21:50 -0700

    front page 'done' need to complete footer with content

--- vics-sister-bus/main.js
+++ vics-sister-bus/main.js
@@ -1,8 +1,28 @@
+anime({
+    targets: '.jumbo-screen .logo',
+    translateX: ['-50%', '-50%'],
+    translateY: ['50%', '-50%'],
+    opacity: [0,1],
+    duration: 1500,
+    direction: 'normal',
+    easing: 'easeInOutQuad',
+});
+
+anime({
+    targets: '.navbar',
+    opacity: [0,1],
+    duration: 2000,
+    direction: 'normal',
+    easing: 'easeInOutQuad',
+})
+
 let openMenu = document.getElementById("open-menu");
 let closeMenu = document.getElementById("close-menu");
 let nav = document.getElementById("menuList");
+let navElements = document.getElementById("menuList").children;
+let container =  document.getElementById("content");
 
-handleMenu = (id) =>
+const handleMenu = (id) =>
 {
     document.getElementById(id).style.display =  "none";
 
@@ -10,11 +30,27 @@ handleMenu = (id) =>
     {
         closeMenu.style.display = "block";
         nav.style.visibility = "visible";
+        document.body.style.overflow = 'hidden';
+
+        anime({
+            targets: navElements,
+            opacity: {
+                value: [0,1],
+                duration: 1000,
+            },
+            translateY: {
+                value: ['0%', '-80%'],
+                duration: 700,
+            },
+            direction: 'normal',
+            easing: 'easeInOutQuad',
+        });
     }
     else 
     {
         openMenu.style.display = "block";
         nav.style.visibility = "hidden";
+        document.body.style.overflow = 'visible';
     }
 
     //window.getComputedStyle(document.getElementById(id)).display
@@ -23,26 +59,37 @@ handleMenu = (id) =>
 var Pwidth = 0;
 window.onresize = resize;
 
-function resize()
+function resize() //reset content if size is for whatever reason changed
 {
     var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
 
     console.log("P: " + Pwidth + "  W: " + width);
 
-    if ( width > 700 && Pwidth < 700 )
+    if ( width > 800 && Pwidth < 800 ) //bigger sizes
     {
         openMenu.style.display = "none";
         closeMenu.style.display = "none";
         nav.style.visibility = "visible";
+        document.body.style.overflow = 'visible';
 
         Pwidth = width;
     }
-    else if ( width < 700  && Pwidth > 700 )
+    else if ( width < 800  && Pwidth > 800 ) // mobile size
     {
         openMenu.style.display = "block";
         closeMenu.style.display = "none";
         nav.style.visibility = "hidden";
+        document.body.style.overflow = 'visible';
 
         Pwidth = width;
     }
 }
+
+
+const elementInView = (el) => {
+    const elementTop = el.getBoundingClientRect().top;
+   
+    return (
+      elementTop <= (window.innerHeight || document.documentElement.clientHeight)
+    );
+  };
